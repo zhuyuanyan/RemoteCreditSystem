5538c0ed5456931f68d3cd0fed829b855fe7b514
qkhj <qk_hejia@163.com>
2015-06-27 17:26:38 +0800

    【add】添加评估

--- RemoteCreditSystem/models/system_usage/Rcs_Application_Advice.py
+++ RemoteCreditSystem/models/system_usage/Rcs_Application_Advice.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 from RemoteCreditSystem import db
 import datetime
+from flask.ext.login import current_user
 
 # 进件评估结论表
 class Rcs_Application_Advice(db.Model):
@@ -10,14 +11,17 @@ class Rcs_Application_Advice(db.Model):
     approve_advice = db.Column(db.String(255))
     approve_result = db.Column(db.String(32))
     approve_ed = db.Column(db.String(32))
-    user_id = db.Column(db.String(32))
+    user_id = db.Column(db.Integer, db.ForeignKey('rcs_user.id'))
 
-    def __init__(self, application_id,approve_advice,approve_result,approve_ed,user_id):
+    #外键
+    rcs_advice_ibfk_1 = db.relationship('User', backref='rcs_advice_ibfk_1')
+
+    def __init__(self, application_id,approve_advice,approve_result,approve_ed):
         self.application_id = application_id
         self.approve_advice = approve_advice
         self.approve_result = approve_result
         self.approve_ed = approve_ed
-        self.user_id = user_id
+        self.user_id = current_user.id
    
     def add(self):
         db.session.add(self)
--- RemoteCreditSystem/views/process/dqdc.py
+++ RemoteCreditSystem/views/process/dqdc.py
@@ -17,7 +17,8 @@ from RemoteCreditSystem.models import SC_Guarantees_For_Others
 from RemoteCreditSystem.models import SC_Riskanalysis_And_Findings
 from RemoteCreditSystem.models.credit_data.sc_stock import SC_Stock
 from RemoteCreditSystem.models.credit_data.sc_individual_customer import SC_Individual_Customer
-
+from RemoteCreditSystem.models.system_usage.Rcs_Application_Advice import Rcs_Application_Advice
+from RemoteCreditSystem.models.system_usage.Rcs_Application_Expert import Rcs_Application_Expert
 
 
 
@@ -44,3 +45,85 @@ def dqdcXed_jbqk(id):
     return render_template("Process/dqdc/dqdcXed_jbqk.html",id=id,customer=customer,loan_apply=loan_apply,
         apply_info=apply_info,loan_purpose=loan_purpose,credit_history=credit_history,
         guarantees_for_others=guarantees_for_others,riskanalysis_and_findings=riskanalysis_and_findings)
+
+# 编辑贷款调查——小额贷款(基本情况)
+@app.route('/Process/dqdc/edit_dqdcXed_jbqk/<int:id>', methods=['POST'])
+def edit_dqdcXed_jbqk(id):
+    try:
+        riskanalysis_and_findings = SC_Riskanalysis_And_Findings.query.filter_by(loan_apply_id=id).first()
+        if riskanalysis_and_findings:
+            riskanalysis_and_findings.analysis_conclusion = request.form['analysis_conclusion']
+            riskanalysis_and_findings.other_deliberations = request.form['other_deliberations']
+            riskanalysis_and_findings.positive = request.form['positive']
+            riskanalysis_and_findings.opposite = request.form['opposite']
+
+            verification_list = request.form.getlist('verification')
+            riskanalysis_and_findings.verification = 0
+            # 循环获取表单
+            for i in range(len(verification_list)):
+                riskanalysis_and_findings.verification += int(verification_list[i])
+            
+            riskanalysis_and_findings.others = request.form['others']
+            riskanalysis_and_findings.bool_grant = request.form['bool_grant']
+            if request.form['bool_grant'] == '1':
+                riskanalysis_and_findings.recommended_way_of_security = request.form['recommended_way_of_security']
+                riskanalysis_and_findings.income_ratio = request.form['income_ratio']
+                riskanalysis_and_findings.amount = request.form['amount']
+                riskanalysis_and_findings.deadline = request.form['deadline']
+                riskanalysis_and_findings.rates = request.form['rates']
+                riskanalysis_and_findings.monthly_repayment = request.form['monthly_repayment']
+                riskanalysis_and_findings.approve_reason = request.form['approve_reason']
+            else:
+                riskanalysis_and_findings.refuse_reason = request.form['refuse_reason']
+        else:
+            verification_list = request.form.getlist('verification')
+            verification_value = 0
+            # 循环获取表单
+            for i in range(len(verification_list)):
+                verification_value += int(verification_list[i])
+
+            SC_Riskanalysis_And_Findings(id,request.form['analysis_conclusion'],
+                request.form['recommended_way_of_security'],request.form['income_ratio'],
+                verification_value,request.form['others'],
+                request.form['bool_grant'],request.form['amount'],
+                request.form['deadline'],request.form['rates'],
+                request.form['monthly_repayment'],request.form['approve_reason'],
+                request.form['refuse_reason'],request.form['other_deliberations'],
+                request.form['positive'],request.form['opposite']).add()
+
+        loan_apply = SC_Loan_Apply.query.filter_by(id=id).first()
+        # loan_apply.process_status = PROCESS_STATUS_DQDC
+
+        #专家建议保存
+        advice = Rcs_Application_Advice.query.filter_by(application_id=id,user_id=current_user.id).first()
+        if advice:
+            #同意
+            if request.form['bool_grant'] == '1':
+                advice.approve_advice= request.form['approve_reason']
+                advice.approve_result="1"
+                advice.approve_ed = request.form['amount']
+            else:
+                advice.approve_advice= request.form['refuse_reason']
+                advice.approve_result="2"
+        else:
+            #同意
+            if request.form['bool_grant'] == '1':
+                Rcs_Application_Advice(id,request.form['approve_reason'],"1",request.form['amount']).add()
+            else:
+                Rcs_Application_Advice(id,request.form['refuse_reason'],"2","").add()
+        #专家进件关系表设置为已评估
+        expert = Rcs_Application_Expert.query.filter_by(application_id=id,expert_id=current_user.id).first()
+        if expert:
+            expert.operate="1"
+        # 事务提交
+        db.session.commit()
+        # 消息闪现
+        flash('保存成功','success')
+    except:
+        # 回滚
+        db.session.rollback()
+        logger.exception('exception')
+        # 消息闪现
+        flash('保存失败','error')
+
+    return redirect("Process/dqdc/dqdcXed_jbqk/"+str(id))
