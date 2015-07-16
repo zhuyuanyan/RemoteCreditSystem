91870bc5782fc051a42a133ec700c879db8492a9
qkhj <qk_hejia@163.com>
2015-07-16 13:36:43 +0800

    【modify】标准还款计算

--- RemoteCreditSystem/logic/xxlr/xxlr.py
+++ RemoteCreditSystem/logic/xxlr/xxlr.py
@@ -10,13 +10,19 @@ from RemoteCreditSystem.config import logger
 from RemoteCreditSystem.models.system_usage.SC_Application_Lrb import SC_Application_Lrb
 from RemoteCreditSystem.models.system_usage.SC_Application_Xjllb import SC_Application_Xjllb
 from RemoteCreditSystem.models.system_usage.SC_Application_Zcfzb import SC_Application_Zcfzb
+from RemoteCreditSystem.models.system_usage.Rcs_Application_Info import Rcs_Application_Info
+from RemoteCreditSystem.models.system_usage.Rcs_Application_Score import Rcs_Application_Score
+from RemoteCreditSystem.models.system_usage.Rcs_Parameter import Rcs_Parameter
 
 def xxlr_zcfzb_bz_save(loan_apply_id,request):
     try:
         SC_Application_Zcfzb.query.filter_by(loan_apply_id=loan_apply_id).delete()
         db.session.flush()
         SC_Application_Zcfzb(loan_apply_id,request.form['content']).add()
-        
+        info = Rcs_Application_Info.query.filter_by(id=loan_apply_id).first()
+        if info:
+            #设置为标准模型
+            info.model_type=2
         db.session.commit()
     except:
         # 回滚
@@ -24,13 +30,35 @@ def xxlr_zcfzb_bz_save(loan_apply_id,request):
         #抛出异常到view view负责打印
         raise
     
-def xxlr_lrb_bz_save(loan_apply_id,request):
+def xxlr_lrb_bz_save(loan_apply_id,request,value1):
     try:
         SC_Application_Lrb.query.filter_by(loan_apply_id=loan_apply_id).delete()
         db.session.flush()
         
         SC_Application_Lrb(loan_apply_id,request.form['content']).add()
-        
+        #月利润
+        pet = value1
+        info = Rcs_Application_Info.query.filter_by(id=loan_apply_id).first()
+        if info:
+            #设置为标准模型
+            info.model_type=2
+        #获取还款能力参数配置数据
+        parm = Rcs_Parameter.query.filter_by(parameter_name='hknl').first()
+        if parm:
+            print "----------2"
+            value = parm.parameter_value.split(',')
+            value = value[140]
+            #获取标准还款能力数据
+            score = Rcs_Application_Score.query.filter_by(application_id=loan_apply_id).first()
+            print "-------1"
+            print value
+            print pet
+            if score:
+                score.hknl_score = float(value)*float(pet)
+            else:
+                print "----------------"
+                Rcs_Application_Score(loan_apply_id,'',float(value)*float(pet),'','','','').add()
+
         db.session.commit()
     except:
         # 回滚
@@ -44,7 +72,10 @@ def xxlr_xjllb_bz_save(loan_apply_id,request):
         db.session.flush()
         
         SC_Application_Xjllb(loan_apply_id,request.form['content']).add()
-        
+        info = Rcs_Application_Info.query.filter_by(id=loan_apply_id).first()
+        if info:
+            #设置为标准模型
+            info.model_type=2
         db.session.commit()
     except:
         # 回滚
