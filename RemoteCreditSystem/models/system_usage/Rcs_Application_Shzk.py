ed27fb161d653cf49f2bc3841957344872938598
qkhj <qk_hejia@163.com>
2015-07-28 10:47:31 +0800

    【modify】评估页面计算，一些bug修复

--- RemoteCreditSystem/models/system_usage/Rcs_Application_Shzk.py
+++ RemoteCreditSystem/models/system_usage/Rcs_Application_Shzk.py
@@ -11,14 +11,16 @@ class Rcs_Application_Shzk(db.Model):
     value_1 = db.Column(db.String(5000))
     value_2 = db.Column(db.String(5000))
     value_3 = db.Column(db.String(5000))
+    value_select = db.Column(db.BLOB)
     create_user = db.Column(db.String(32))
     create_time = db.Column(db.DateTime)
 
-    def __init__(self, application_id,value_1,value_2,value_3):
+    def __init__(self, application_id,value_1,value_2,value_3,value_select):
         self.application_id = application_id
         self.value_1 = value_1
         self.value_2 = value_2
         self.value_3 = value_3
+        self.value_select = value_select
         self.create_user = current_user.id
         self.create_time = datetime.datetime.now()
    
